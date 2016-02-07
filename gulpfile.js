7448bfb49828705e2e7b00f84876a996be9eedb0
Alexei <hello@alexei.ro>
2016-02-07 17:45:26 +0200

    Generates sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
 'use strict'
 
-var gulp   = require('gulp'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename')
+var gulp        = require('gulp'),
+    uglify      = require('gulp-uglify'),
+    rename      = require('gulp-rename'),
+    sourcemaps  = require('gulp-sourcemaps')
 
 gulp.task('dist', function() {
     return gulp.src([
             'src/*.js'
         ])
+        .pipe(sourcemaps.init())
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('dist'))
 })
 
