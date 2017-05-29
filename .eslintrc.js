7895e11dcd90f29cad0d372a5297ea8fdcba0d28
Alexei <hello@alexei.ro>
2017-05-29 18:26:18 +0300

    Fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,9 @@ module.exports = {
         es6: true
     },
     rules: {
-        'no-control-regex':0,
-        'no-undef':0,
-        'no-unused-vars':0,
+        'no-control-regex': 0,
+        'no-undef': 0,
+        'no-unused-vars': 0,
         'no-console': 0,
         indent: ['error', 4],
         'quote-props': ['error', 'as-needed']
